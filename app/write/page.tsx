e4bc4317d42ae273e21ae426dd62f359636a958b
jaewan kim <green23616@gmail.com>
2023-11-15 12:59:01 +0900

    15

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -7,9 +7,8 @@ interface formType{
   content: string;
 }
 
-import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useCustomSession } from "../sessions";
 
 export default function Write(){
@@ -59,7 +58,7 @@ export default function Write(){
 
           <Link href="/" className="w-20 h-10 bg-yellow-400 text-white font-bold px-4 py-2 rounded shadow-md hover:text-black hover:bg-orange-500 transition-all duration-150 focus:outline-none text-center mr-5 mt-5">취소</Link>
           <form method="post" onSubmit={submitEvent}>
-            <input type="text" name="name" value={session && session.user.name} placeholder="이름을 입력해주세요" onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border w-1/2 outline-none"/>
+            <input type="text" name="name" defaultValue={session?.user?.email ?? ''} placeholder="이름을 입력해주세요" onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border w-1/2 outline-none"/>
             <input type="text" name="title" defaultValue={formData.title} placeholder="제목을 입력해주세요"onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border w-1/2 outline-none" />
             <textarea name="content" defaultValue={formData.content} placeholder="내용을 입력해주세요" onChange={changeEvent} className="shadow text-gray-700 text-sm mb-2 border w-full h-20 outline-none"></textarea>
             <button className='bg-green-400 text-white font-bold px-6 py-2 rounded shadow-md hover:text-black hover:bg-green-500 transition-all duration-150 focus:outline-none absolute top-[24px] ml-5'>
